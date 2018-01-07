dd457a31b08cc7d3a9af5487edb264bbe89aa2b5
ma34s <ma34s.sss@gmail.com>
2018-01-08 00:27:59 +0900

    now ready for twrp build
    Change-Id: Ic659ff002e95ab36af5564ac07b22829ec182263

--- build_twrp.sh
+++ build_twrp.sh
@@ -7,6 +7,19 @@
 PRODUCT=$1
 
 export TARGET_RECOVERY=twrp
-export TWRP_VERSION=`cat $(pwd)/bootable/recovery-twrp/variables.h |grep TW_VERSION_STR | awk '{print $3;}' |sed -e s/\"//g`
+export WITH_TWRP=true
+export PLATFORM_SDK_VERSION=26
+export TWRP_VERSION=`grep "#define TW_MAIN_VERSION_STR" bootable/recovery-twrp/variables.h | awk '{print $3;}' |sed -e s/\"//g`
 export OUT_DIR=$(pwd)/out_twrp
+
+
 ./build.sh $PRODUCT recoveryimage ${@:2}
+
+if [ ! "${OUT_DIR}" ]; then
+  cd out/target/product/${PRODUCT}
+else
+  cd ${OUT_DIR}/target/product/${PRODUCT}
+fi
+tar cf twrp-${TWRP_VERSION}-0-${PRODUCT}.img.tar recovery.img
+cd -
+
